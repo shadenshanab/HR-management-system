9bd1cebf7645e03b03513e8334e7be3c4eb2374d
Shaden <shadenshanab2@gmail.com>
2022-08-10 20:52:45 +0300

    version 2

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ function Employee(fullName, department, level, img){
     this.salary = salaryGenerator(level)
     allEmployees.push(this);
 }
-
 const employee1 = new Employee("Ghazi Samer", "Administration", "Senior", "./assets/ghazi.jpg");
 const employee2 = new Employee("Lana Ali", "Finance", "Senior", "./assets/lana.jpg");
 const employee3 = new Employee("Tamara Ayoub", "Marketing", "Senior", "./assets/tamara.jpg");
@@ -18,27 +17,25 @@ const employee6 = new Employee("Rana Saleh", "Development", "Junior", "./assets/
 const employee7 = new Employee("Hadi Ahmad", "Finance", "Mid-Senior", "./assets/hadi.jpg");
 
 Employee.prototype.printMenu = function () {
-    document.write(`Employee name : ${this.fullName}`)
-    document.write(`Department : ${this.department}`)
-    document.write(`Employee salary: ${this.salary}`)
-    
-    
+    document.write(`<p>Employee name : ${this.fullName}</p>`)
+    document.write(`<p>Department : ${this.department}</p>`)
+    document.write(`<p>Employee salary: ${this.salary}</p>`)
 }
 
-console.log("Employee name: " + employee1.fullName + "\n" + "Employee salary: " + employee1.salary);
-console.log("Employee name: " + employee2.fullName + "\n" + "Employee salary: " + employee2.salary);
-console.log("Employee name: " + employee3.fullName + "\n" + "Employee salary: " + employee3.salary);
-console.log("Employee name: " + employee4.fullName + "\n" + "Employee salary: " + employee4.salary);
-console.log("Employee name: " + employee5.fullName + "\n" + "Employee salary: " + employee5.salary);
-console.log("Employee name: " + employee6.fullName + "\n" + "Employee salary: " + employee6.salary);
-console.log("Employee name: " + employee7.fullName + "\n" + "Employee salary: " + employee7.salary);
+ console.log("Employee name: " + employee1.fullName + "\n" + "Employee salary: " + employee1.salary);
+ console.log("Employee name: " + employee2.fullName + "\n" + "Employee salary: " + employee2.salary);
+ console.log("Employee name: " + employee3.fullName + "\n" + "Employee salary: " + employee3.salary);
+ console.log("Employee name: " + employee4.fullName + "\n" + "Employee salary: " + employee4.salary);
+ console.log("Employee name: " + employee5.fullName + "\n" + "Employee salary: " + employee5.salary);
+ console.log("Employee name: " + employee6.fullName + "\n" + "Employee salary: " + employee6.salary);
+ console.log("Employee name: " + employee7.fullName + "\n" + "Employee salary: " + employee7.salary);
 
 var counter = 1000;
-
 
 function IDGenerator(){
     return counter++ ;
 }
+
 function salaryGenerator(level){
     if(level === "Senior"){
         let min = 1500;
@@ -53,5 +50,9 @@ function salaryGenerator(level){
         let max = 1000;
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+}
+
+ for (let i = 0; i < allEmployees.length; i++){
+    allEmployees[i].printMenu();
 }
 
